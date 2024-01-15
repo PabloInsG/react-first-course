f21ab299944cb3c66acfc0ab6e89a4695edaa0ca
Pablo <pablo.insua.gerpe@gmail.com>
2024-01-15 17:23:38 +0100

    first commit

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(
+  <div>My wishlist from React</div>,
+  document.getElementById('root'),
+);
